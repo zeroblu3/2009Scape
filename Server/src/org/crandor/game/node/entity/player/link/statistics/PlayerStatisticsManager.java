beb8a13338134ac02de959b85b274b83e938d0a7
jamix77 <>
2020-04-10 02:58:17 +0100

    The good stuff!
    1 click bank booth
    pat system
    crest data
    removed some weird stuff too.

--- Server/src/org/crandor/game/node/entity/player/link/statistics/PlayerStatisticsManager.java
+++ Server/src/org/crandor/game/node/entity/player/link/statistics/PlayerStatisticsManager.java
@@ -3,11 +3,19 @@ package org.crandor.game.node.entity.player.link.statistics;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 
+import org.crandor.game.component.Component;
+import org.crandor.game.content.skill.Skills;
 import org.crandor.game.node.entity.player.Player;
+import org.crandor.game.node.entity.player.info.Rights;
 import org.crandor.game.node.entity.player.info.login.SavingModule;
+import org.crandor.game.node.entity.player.link.IronmanMode;
+import org.crandor.game.node.entity.player.link.music.MusicEntry;
+import org.crandor.game.node.entity.player.link.quest.Quest;
+import org.crandor.game.node.entity.player.link.quest.QuestRepository;
+import org.crandor.tools.StringUtils;
 
 /**
- * Stuff
+ * statistics manager duh
  * @author jamix77
  *
  */
@@ -25,15 +33,20 @@ public class PlayerStatisticsManager implements SavingModule {
 	CLUES_COMPLETED,
 	ENTITIES_KILLED,
 	DEATHS,
-	LOGS_OBTAINED;
+	LOGS_OBTAINED,
+	PATS;
 
 	
 	/**
      * The player instance for this manager.
      */
     private final Player player;
-    
 
+    /**
+     * 
+     * Constructs a new @{Code PlayerStatisticsManager} object.
+     * @param player
+     */
 	public PlayerStatisticsManager(Player player) {
 		this.player = player;
 		if (!player.isArtificial())
@@ -47,6 +60,7 @@ public class PlayerStatisticsManager implements SavingModule {
 		LOGS_OBTAINED = new Statistic(player,this);
 		ENTITIES_KILLED = new Statistic(player,this);
 		DEATHS = new Statistic(player,this);
+		PATS = new Statistic(player,this);
 	}
 
 	@Override
@@ -71,6 +85,50 @@ public class PlayerStatisticsManager implements SavingModule {
 			}
 		}
 		
+	}
+	
+	public static void sendHiscore(Player player, Player target) {
+		if (player.getInterfaceManager().isOpened()) {
+			player.sendMessage("Finish what you're currently doing.");
+			return;
+		}
+		player.getInterfaceManager().open(new Component(275));
+		//CLear old data
+		for (int i = 0; i < 311; i++) {
+			player.getPacketDispatch().sendString("", 275, i);
+		}
+		// Title
+		//14 Ult IM
+		//13 IM
+		//15 HCIM
+		player.getPacketDispatch().sendString("" + (target.getRights() == Rights.ADMINISTRATOR ? "<img=1>" : (target.getRights() == Rights.PLAYER_MODERATOR ? "<img=0>" : (target.getIronmanManager().getMode() == IronmanMode.STANDARD ? "<img=13>" : (target.getIronmanManager().getMode() == IronmanMode.ULTIMATE ? "<img=14>" : (target.getIronmanManager().getMode() == IronmanMode.HARDCORE ? "<img=15>" : ""))))) + "<col=ae1515>" + target.getUsername() + "</col>'s stats.", 275, 2);
+
+		// Content
+		int lineId = 11;
+		player.getPacketDispatch().sendString("Total level: " + target.getSkills().getTotalLevel(), 275, lineId++);
+		player.getPacketDispatch().sendString("Total xp: " + StringUtils.getFormattedNumber(target.getSkills().getTotalXp()), 275, lineId++);
+		for (int i = 0; i < Skills.SKILL_NAME.length; i++) {
+			player.getPacketDispatch().sendString("" + Skills.SKILL_NAME[i] + ": " + target.getSkills().getStaticLevel(i) + "  (" + StringUtils.getFormattedNumber((int) Math.round(target.getSkills().getExperience(i))) + ")", 275, lineId++);
+		}
+		
+		//stats
+		player.getPacketDispatch().sendString("Music tracks unlocked: " +  target.getMusicPlayer().getUnlocked().size() + "/" + MusicEntry.getSongs().size(), 275, lineId++);
+		player.getPacketDispatch().sendString("Clue scrolls completed: " +  target.getStatisticsManager().getCLUES_COMPLETED().getStatisticalAmount(), 275, lineId++);
+		player.getPacketDispatch().sendString("Slayer tasks completed: " + target.getSlayer().getTotalTasks(),275,lineId++);
+		player.getPacketDispatch().sendString("Al kharid passes: " + target.getStatisticsManager().getAL_KHARID_GATE_PASSES().getStatisticalAmount(), 275, lineId++);
+		player.getPacketDispatch().sendString("Enemies killed: " +  target.getStatisticsManager().getENTITIES_KILLED().getStatisticalAmount(), 275, lineId++);
+		player.getPacketDispatch().sendString("Logs chopped: " +  target.getStatisticsManager().getLOGS_OBTAINED().getStatisticalAmount(), 275, lineId++);
+		player.getPacketDispatch().sendString("Flax picked: " +  target.getStatisticsManager().getFLAX_PICKED().getStatisticalAmount(), 275, lineId++);
+		player.getPacketDispatch().sendString("Deaths: " +  target.getStatisticsManager().getDEATHS().getStatisticalAmount(), 275, lineId++);
+		player.getPacketDispatch().sendString("Pats: " +  target.getStatisticsManager().getPATS().getStatisticalAmount(), 275, lineId++);
+		
+		//quests
+		player.getPacketDispatch().sendString("", 275, lineId++);
+		player.getPacketDispatch().sendString("<u><col=0000FF>Quests Completed:", 275, lineId++);
+		for (Quest q : QuestRepository.getQuests().values()) {
+			player.getPacketDispatch().sendString("" + (q.isCompleted(target) ? "<col=00FF00>" : "<col=ae1515>") + q.getName() + " ", 275, lineId++);
+		}
+	
 	}
 	
 	/**
@@ -109,7 +167,9 @@ public class PlayerStatisticsManager implements SavingModule {
 	public Statistic getDEATHS() {
 		return DEATHS;
 	}
-	
-	
 
+	public Statistic getPATS() {
+		return PATS;
+	}
+	
 }
