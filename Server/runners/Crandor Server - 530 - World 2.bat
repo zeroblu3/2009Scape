4de51cb25a1c9035383defcda27a5b86d4bc3cd2
The Celery Man <61421472+Ceikry@users.noreply.github.com>
2020-05-08 05:06:30 -0500

    Reorganization & Cleanup (#381)
    * Compressed the exported sql databases.
    
    * Changed out for a different compression format
    
    * Fixed a shift-drop dupe
    
    * closes #370
    
    * closes #359
    
    * Reorganized the Server folder a good bit...
    
    * updated the .bat files to work with the new Server layout
    
    * removed the backups from the repo. No reason to have them.

---
+++ Server/runners/Crandor Server - 530 - World 2.bat
@@ -0,0 +1,4 @@
+@echo off
+@title World 2
+java -server -Xms512m -Xmx1536m -XX:NewSize=32m -XX:MaxPermSize=128m -XX:+UseConcMarkSweepGC -XX:+ExplicitGCInvokesConcurrent -XX:+AggressiveOpts -cp ../bin;../data/libs/*;../data/libs/slf4j/*; org.crandor.Server ../worldprops/server2.properties
+pause
