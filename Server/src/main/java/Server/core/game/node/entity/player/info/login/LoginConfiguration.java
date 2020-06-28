cf229013d931b8abd0704243efb601d21498dd44
The Celery Man <61421472+Ceikry@users.noreply.github.com>
2020-06-28 00:52:35 -0500

    Development (#430)
    * added a few more shops
    
    * added Drops plugin type
    
    * Fixed female/male HAM
    
    * added shilo underground gem mine
    
    * added shilo underground gem mine
    
    * fixed antifire potions
    
    * added shilo underground gem mine
    
    * fixed small bug
    
    * Update client.conf

--- Server/src/main/java/Server/core/game/node/entity/player/info/login/LoginConfiguration.java
+++ Server/src/main/java/Server/core/game/node/entity/player/info/login/LoginConfiguration.java
@@ -101,7 +101,7 @@ public final class LoginConfiguration {
         player.getConfigManager().reset();
         sendGameConfiguration(player);
         Repository.getLobbyPlayers().remove(player);
-        Repository.getPlayerNames().put(player.getUsername(),player);
+        //Repository.getPlayerNames().putIfAbsent(player.getUsername(),player);
         player.setPlaying(true);
         UpdateSequence.getRenderablePlayers().add(player);
         RegionManager.move(player);
