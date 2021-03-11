0525196130a60445b239e68374f1e8d6aae18380
ijustlovemath <jcdejournett@gmail.com>
2021-03-10 23:41:27 -0500

    Fixing build issues in the last PR (#893)
    * added barrels with swamp tar as they existed when Lunar quest was added
    
    * fixing minor typos that failed the build
    
    * missing parens like a dummy
    
    * adding npc package
    
    * actually returning the plugin we create

--- Server/src/main/java/Server/core/game/interaction/object/LadyTayOptionPlugin.java
+++ Server/src/main/java/Server/core/game/interaction/object/LadyTayOptionPlugin.java
@@ -5,6 +5,8 @@ import core.cache.def.impl.ObjectDefinition;
 import core.game.component.Component;
 import core.game.interaction.OptionHandler;
 import core.game.node.Node;
+import core.game.node.item.Item;
+import core.game.node.entity.npc.NPC;
 import core.game.node.entity.player.Player;
 import core.game.node.object.GameObject;
 import core.plugin.Initializable;
@@ -15,12 +17,14 @@ import core.plugin.Plugin;
  * @author f00b
  */
 @Initializable
-public class LadyTayOptionsPlugin extends OptionHandler {
+public class LadyTayOptionPlugin extends OptionHandler {
+
 
     @Override
     public Plugin<Object> newInstance(Object arg) throws Throwable {
         /* the tar barrels on the lower deck of the Lady Zay */
         ObjectDefinition.forId(16860).getHandlers().put("option:take-from", this);
+        return this;
     }
 
     @Override
@@ -30,7 +34,7 @@ public class LadyTayOptionsPlugin extends OptionHandler {
         switch (option) {
         case "take-from":
             if (id == 16860) {
-                if (!player.getInventory.hasSpaceFor(add)) {
+                if (!player.getInventory().hasSpaceFor(tar)) {
                     player.getPacketDispatch().sendMessage("Not enough space in your inventory!");
                     return true;
                 }
